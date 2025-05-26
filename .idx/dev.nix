66fdb726561be79d394271f59f35e4f5b1dd5a64
adhityatriwahyu <adhityatriwahyu@gmail.com>
2025-05-26 03:26:24 +0000

    update login page

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,6 +1,6 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://firebase.google.com/docs/studio/customize-workspace
-{ pkgs, ... }: {
+{ ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
 
@@ -10,6 +10,7 @@
     # pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
+    # pkgs.bun
     # pkgs.nodePackages.nodemon
   ];
 
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="flex flex-col flex-1 items-center justify-center h-[100dvh]">
-        <UCard class="flex gap-4">
-            <UInput type="email" v-model="email" />
-            <UButton @click="signInWithOtp">
+        <UCard class="w-[300px]">
+          <template #header>
+            Login Form
+          </template>
+          <div class="flex flex-col gap-2">
+            <UFormField label="Email" class="w-[100%]">
+              <UInput size="xl" type="email" v-model="email" class="w-[100%]" />
+            </UFormField>
+            <UButton class="mt-4" size="xl" @click="signInWithOtp">
                 Sign in with OTP
             </UButton>
+          </div>
         </UCard>
     </div>
 </template>
